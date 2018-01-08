2d974e17aca80150b7fd0d775ac64471ed7533bb
KingJA <kingjavip@gmail.com>
2018-01-08 17:07:46 +0800

    Initial commit

---
+++ src/test/java/com/immoc/LoggertTest.java
@@ -0,0 +1,26 @@
+package com.immoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Description：TODO
+ * Create Time：2018/1/8 16:11
+ * Author:KingJA
+ * Email:kingjavip@gmail.com
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggertTest {
+
+    private final Logger logger= LoggerFactory.getLogger(LoggertTest.class);
+
+    @Test
+    private void test1() {
+
+    }
+}
